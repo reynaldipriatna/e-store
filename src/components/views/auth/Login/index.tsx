5719dd909e6643e7ee992072c6ae5b775ad2e67b
Reynaldi Priatna <rey.twenty.one@gmail.com>
2023-12-20 21:43:46 +0700

    feat: instance fetch axios

--- src/components/views/auth/Login/index.tsx
+++ src/components/views/auth/Login/index.tsx
@@ -5,6 +5,7 @@ import { FormEvent, useState } from "react";
 import { signIn } from "next-auth/react";
 import Input from "@/components/ui/Input";
 import Button from "@/components/ui/Button";
+import AuthLayout from "@/components/layouts/AuthLayout";
 
 const LoginView = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -39,43 +40,39 @@ const LoginView = () => {
   };
 
   return (
-    <div className={styles.login}>
-      <h1 className={styles.login__title}>Login</h1>
-      {error && <p className={styles.login__error}>{error}</p>}
-      <div className={styles.login__form}>
-        <form onSubmit={handleSubmit}>
-          <Input label="Email" name="email" type="email" placeholder="Email" />
-          <Input
-            label="Password"
-            name="password"
-            type="password"
-            placeholder="Password"
-          />
-          <Button
-            type="submit"
-            variant="primary"
-            className={styles.login__form__button}
-          >
-            {isLoading ? "Loading..." : "Login"}
-          </Button>
-        </form>
-        <hr className={styles.login__form__devider} />
-        <div>
-          <Button
-            type="button"
-            onClick={() => signIn("google", { callbackUrl, redirect: false })}
-            className={styles.login__form__other__button}
-          >
-            <i className="bx bxl-google" />
-            Login With Google
-          </Button>
-        </div>
+    <AuthLayout
+      title="Login"
+      link="/auth/register"
+      linkText="Don't Have an account? Sign up "
+    >
+      <form onSubmit={handleSubmit}>
+        <Input label="Email" name="email" type="email" placeholder="Email" />
+        <Input
+          label="Password"
+          name="password"
+          type="password"
+          placeholder="Password"
+        />
+        <Button
+          type="submit"
+          variant="primary"
+          className={styles.login__button}
+        >
+          {isLoading ? "Loading..." : "Login"}
+        </Button>
+      </form>
+      <hr className={styles.login__devider} />
+      <div>
+        <Button
+          type="button"
+          onClick={() => signIn("google", { callbackUrl, redirect: false })}
+          className={styles.login__other__button}
+        >
+          <i className="bx bxl-google" />
+          Login With Google
+        </Button>
       </div>
-      <p className={styles.login__link}>
-        Don{"'"}t Have an account? Sign up{" "}
-        <Link href="/auth/register">Here</Link>
-      </p>
-    </div>
+    </AuthLayout>
   );
 };
 
